1dcaf275c7bcdc485c74866bd94e881050f7557d
kevin-ar-cmd <otienokevin1000090@gmail.com>
2025-01-24 16:54:57 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>
+      Hi, I am KEVIN JUMA, a passionate developer specializing in web development.
+      I love creating clean and efficient web applications.
+    </p>
+  </section>
+);
+
+export default About;
